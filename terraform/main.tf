9e43144a4082dbb2bb715b252f8bd8651e308efb
Amol Shivankar <amolshivankar2108@gmail.com>
2026-04-25 14:37:57 +0530

    new line added in subnet resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,6 +28,8 @@ resource "aws_subnet" "public_subnet" {
     cidr_block= "10.0.1.0/24"
     map_public_ip_on_launch = true
 
+    availability_zone = "ap-south-1a" 
+
     tags = {
         Name= "public_subnet"
     }
